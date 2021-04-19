414151f285b16f006076b559eeb00956f2f33c53
Martinuxis <82836085+Martinuxis@users.noreply.github.com>
2021-04-19 19:50:10 +0200

    Update Program.cs

--- TestSpeed/Program.cs
+++ TestSpeed/Program.cs
@@ -116,7 +116,7 @@ namespace TestSpeed
             stopWatch2.Stop();
             TimeSpan ts2 = stopWatch.Elapsed;
 
-            Console.WriteLine("RunTime Switch: " + ts2);
+            Console.WriteLine("Bendr je Bůh: -just a test-" + ts2);
             
         }
     }
